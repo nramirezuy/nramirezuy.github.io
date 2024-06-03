283db9d1b2085ec233543fd549a40f94e6d4cc37
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-06-03 15:57:25 -0300

    feat(website): Start empty pelican website

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+AUTHOR = "Nicolás Ramírez"
+SITENAME = "nico's site"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "America/Montevideo"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
